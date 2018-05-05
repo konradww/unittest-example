89192438b47792d661097b60172616369b3b6dc0
Konrad <marek.dudek@protonmail.ch>
2018-05-05 17:25:23 +0200

    I added method setUp

--- test.py
+++ test.py
@@ -2,41 +2,50 @@ import unittest
 from employee import Employee
 class TestEmployee(unittest.TestCase):
 
-    def test_email(self):
-        employee1 = Employee('Janusz','Kowalski',10000)
-        employee2 = Employee('Martynka', 'Jasic', 20000)
-        employee3 = Employee('Edward', 'Kolonko', 15000)
+    @classmethod
+    def setUp(self):
+        self.employee1  = Employee('Janusz', 'Kowalski', 10000)
+        self.employee2 = Employee('Martynka', 'Jasic', 20000)
+        self.employee3 = Employee('Edward', 'Kolonko', 15000)
 
-        self.assertEqual(employee1.email,'Janusz.Kowalski@email.com')
-        self.assertEqual(employee2.email, 'Martynka.Jasic@email.com')
-        self.assertEqual(employee3.email, 'Edward.Kolonko@email.com')
+    def tearDown(self):
+        pass
+        
+    def test_email(self):
+        # self.employee1  = Employee('Janusz','Kowalski',10000)
+        # self.employee2 = Employee('Martynka', 'Jasic', 20000)
+        # self.employee3 = Employee('Edward', 'Kolonko', 15000)
+
+        self.assertEqual(self.employee1.email,'Janusz.Kowalski@email.com')
+        self.assertEqual(self.employee2.email, 'Martynka.Jasic@email.com')
+        self.assertEqual(self.employee3.email, 'Edward.Kolonko@email.com')
 
     def test_fullname(self):
-        employee1 = Employee('Janusz', 'Kowalski', 10000)
-        employee2 = Employee('Martynka', 'Jasic', 20000)
-        employee3 = Employee('Edward', 'Kolonko', 15000)
+        # self.employee1  = Employee('Janusz', 'Kowalski', 10000)
+        # self.employee2 = Employee('Martynka', 'Jasic', 20000)
+        # self.employee3 = Employee('Edward', 'Kolonko', 15000)
 
-        self.assertEqual(employee1.fullname, 'Janusz Kowalski')
-        self.assertEqual(employee2.fullname, 'Martynka Jasic')
-        self.assertEqual(employee3.fullname, 'Edward Kolonko')
+        self.assertEqual(self.employee1.fullname, 'Janusz Kowalski')
+        self.assertEqual(self.employee2.fullname, 'Martynka Jasic')
+        self.assertEqual(self.employee3.fullname, 'Edward Kolonko')
 
-        employee1.first = 'Test'
-        employee1.last = 'Test'
+        self.employee1.first = 'Test'
+        self.employee1.last = 'Test'
 
-        self.assertEqual(employee1.fullname, 'Test Test')
+        self.assertEqual(self.employee1.fullname, 'Test Test')
 
     def test_apply_raise(self):
-        employee1 = Employee('Janusz', 'Kowalski', 10000)
-        employee2 = Employee('Martynka', 'Jasic', 20000)
-        employee3 = Employee('Edward', 'Kolonko', 15000)
+        # self.employee1  = Employee('Janusz', 'Kowalski', 10000)
+        # self.employee2 = Employee('Martynka', 'Jasic', 20000)
+        # self.employee3 = Employee('Edward', 'Kolonko', 15000)
 
-        employee1.apply_raise()
-        employee2.apply_raise()
-        employee3.apply_raise()
+        self.employee1.apply_raise()
+        self.employee2.apply_raise()
+        self.employee3.apply_raise()
 
-        self.assertEqual(employee1.pay,11500)
-        self.assertEqual(employee2.pay,23000)
-        self.assertEqual(employee3.pay,17250)
+        self.assertEqual(self.employee1.pay,11500)
+        self.assertEqual(self.employee2.pay,23000)
+        self.assertEqual(self.employee3.pay,17250)
 
 #
 # if __name__ == '__main__':
